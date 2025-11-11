4f3465fd1983571685830ecaa7c263b19c21d4eb
Bogdanpups1 <kapishevskii11@gmail.com>
2025-11-11 09:22:19 +0200

    123

--- Project2/forward_list.cpp
+++ Project2/forward_list.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 
-template <class T>
 
+template <class T>
 struct Element
 {
 	T data;
@@ -12,8 +12,8 @@ struct Element
 template <class T>
 class List
 {
-	T* Head;
-	T* Tail;
+	Element<T>* Head;
+	Element<T>* Tail;
 	int Count;
 
 public:
@@ -24,7 +24,11 @@ public:
 	void DelAll();
 	void Print();
 	int GetCount();
+	void AddByPos(T data, int posit);
+	void DeleteByPos(int posit);
+	int Search(T element);
 };
+
 template <class T>
 List<T>::List()
 {
@@ -36,13 +40,89 @@ List<T>::~List()
 {
 	DelAll();
 }
-
 template <class T>
 int List<T>::GetCount()
 {
 	return Count;
 }
+template<class T>
+void List<T>::AddByPos(T data, int posit)
+{
+	if (posit < 0 || posit > Count)
+	{
+		cout << "Nuh huh,incorrect position" << endl;
+		return;
+	}
+	Element<T>* temp = new Element<T>;
+	temp->data = data;
 
+	if (posit == 0)
+	{
+		temp->Next = Head;
+		Head = temp;
+	}
+	else if (posit == Count)
+	{
+		Tail->Next = temp;
+		temp->Next = NULL;
+		Tail = temp;
+	}
+	else
+	{
+		Element<T>* current = Head;
+		for (int i = 0; i < posit - 1; i++)
+		{
+			current = current->Next;
+		}
+		temp->Next = current->Next;
+		current->Next = temp;
+	}
+	Count++;
+}
+template<class T>
+void List<T>::DeleteByPos(int posit)
+{
+	if (posit < 0 || posit > Count)
+	{
+		cout << "Nuh huh,incorrect position" << endl;
+		return;
+	}
+
+	Element<T>* temp = NULL;
+
+	if (posit == 0)
+	{
+		Del();
+	}
+	else
+	{
+		Element<T>* current = Head;
+		for (int i = 0; i < posit - 1; i++)
+		{
+			current = current->Next;
+		}
+		temp = current->Next;
+		current->Next = temp->Next;
+	}
+	delete temp;
+	Count--;
+}
+template<class T>
+int List<T>::Search(T element)
+{
+	Element<T>* current = Head;
+	int posit = 0;
+	while (current != NULL)
+	{
+		if (current->data == element)
+		{
+			return posit;
+		}
+		current = current->Next;
+		posit++;
+	}
+	return -1;
+}
 template <class T>
 void List<T>::Add(T data)
 {
@@ -50,34 +130,36 @@ void List<T>::Add(T data)
 
 	temp->data = data;
 	temp->Next = NULL;
+
 	if (Head != NULL) {
 		Tail->Next = temp;
 		Tail = temp;
 	}
+
 	else {
 		Head = Tail = temp;
 	}
+	Count++;
 }
-
 template <class T>
 void List<T>::Del()
 {
-	Element* temp = Head;
+	Element<T>* temp = Head;
 	Head = Head->Next;
 	delete temp;
+	Count--;
 }
-
 template <class T>
 void List<T>::DelAll()
 {
 	while (Head != 0)
 		Del();
+	Count--;
 }
-
 template <class T>
 void List<T>::Print()
 {
-	Element* temp = Head;
+	Element<T>* temp = Head;
 	while (temp != 0)
 	{
 		cout << temp->data << " ";
@@ -86,20 +168,36 @@ void List<T>::Print()
 	cout << "\n\n";
 }
 template <class T>
-void main()
+ostream& operator<<(ostream& os, List<T>& obj)
 {
-
-	List<T> lst;
-
-	char s[] = "Hello, World !!!\n";
-	cout << s << "\n\n";
-	int len = strlen(s);
-	for (int i = 0; i < len; i++)
-		lst.Add(s[i]);
-	lst.Print();
-	lst.Del();
-	lst.Del();
-	lst.Del();
-	lst.Print();
+	obj.Print();
+	return os;
 }
 
+int main()
+{
+	srand(unsigned(time(0)));
+
+	List<int> lst;
+
+	for (int i = 0; i < 5; i++)
+		lst.Add(rand() % 10);
+
+	lst.Print();
+	lst.Del();
+	lst.Del();
+	lst.Del();
+	lst.Print();
+	lst.AddByPos(9, 0);
+	lst.Print();
+	lst.AddByPos(52, 2);
+	lst.Print();
+	cout << lst.GetCount() << "\n" << endl;
+	cout << lst.Search(52) << "\n" <<  endl;
+	lst.DeleteByPos(1);
+	lst.Print();
+	lst.DeleteByPos(2);
+	lst.Print();
+	lst.AddByPos(1112, 1);
+	lst.Print();
+}
